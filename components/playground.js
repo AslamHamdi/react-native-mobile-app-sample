2dca3bc9e7cabf299c68f9f7eae1707938f963b3
AslamHamdi <aslam.hamdi@growth-rts.com>
2022-09-09 17:04:53 +0800

    update use fusion chart

--- components/playground.js
+++ components/playground.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { View, Text, StyleSheet, Image, Dimensions, ScrollView, AppRegistry, Platform, processColor } from 'react-native';
 import { ListItem, Button, Icon, Input } from 'react-native-elements'
 import update from 'immutability-helper';
 import _ from 'lodash';
-import { LineChart } from 'react-native-charts-wrapper';
+
+import { WebView } from "react-native-webview";
+import { readAsStringAsync } from "expo-file-system";
+import { useAssets } from "expo-asset";
+import ReactNativeFusionCharts from 'react-native-fusioncharts';
+import {LineChart} from 'react-native-charts-wrapper';
+// import FusionCharts from 'react-native-fusioncharts';
 
 //var LineChart = require('react-native-charts-wrapper').default
 
@@ -11,120 +17,319 @@ import { LineChart } from 'react-native-charts-wrapper';
 const COLOR_PURPLE = processColor('#697dfb');
 
 export default class PlayGround extends React.Component {
-    constructor() {
-        super();
-    
-        this.state = {
-          data: {},
-          xAxis: {},
-          yAxis: {}
-        };
-      }
-    
-      componentDidMount() {
-        const valueRange = 100;
-        const size = 30;
-    
-        this.setState(
-          update(this.state, {
-            xAxis: {
-              $set: {
-                textColor: processColor('red'),
-                textSize: 16,
-                gridColor: processColor('red'),
-                gridLineWidth: 1,
-                axisLineColor: processColor('darkgray'),
-                axisLineWidth: 1.5,
-                gridDashedLine: {
-                  lineLength: 10,
-                  spaceLength: 10
-                },
-                avoidFirstLastClipping: true,
-                position: 'BOTTOM'
-              }
-            },
-            yAxis: {
-              $set: {
-                left: {
-                  drawGridLines: false
-                },
-                right: {
-                  enabled: false
-                }
-              }
-            },
-            data: {
-              $set: {
-                dataSets: [{
-                  values: this._randomYValues(valueRange, size),
-                  label: '',
-                  config: {
-                    lineWidth: 1.5,
-                    drawCircles: false,
-                    drawCubicIntensity: 0.3,
-                    drawCubic: true,
-                    drawHighlightIndicators: false,
-                    color: COLOR_PURPLE,
-                    drawFilled: true,
-                    fillColor: COLOR_PURPLE,
-                    fillAlpha: 90
+  constructor(props) {
+    super(props);
+    //STEP 3 - Chart Configurations
+    const chartConfig = {
+      type: "scrollline2d",
+      width: "100%",
+      height: "400",
+      dataFormat: "json",
+      compactDataMode: 1,
+      dataSource: {
+        "chart": {
+          "theme": "fusion",
+          "caption": "Sales Trends",
+          "subCaption": "(2016 to 2017)",
+          "xAxisName": "Month",
+          "yAxisName": "Revenue",
+          "numberPrefix": "$",
+          "lineThickness": "3",
+          "flatScrollBars": "1",
+          "scrollheight": "10",
+          "numVisiblePlot": "12",
+          "showHoverEffect": "1",
+          "borderColor": "#666666",
+          "showBorder": "1",
+          "borderThickness": "4",
+          //"borderRadius": "20",
+          "borderAlpha": "80",
+          "bgColor": "#232B5D",
+          "bgratio": "60,40",
+          //"bgAlpha": "70,80",
+          "bgAngle": "180"
+      },
+      "categories": [
+          {
+              "category": [
+                  {
+                      "label": "Jan 2016"
+                  },
+                  {
+                      "label": "Feb 2016"
+                  },
+                  {
+                      "label": "Mar 2016"
+                  },
+                  {
+                      "label": "Apr 2016"
+                  },
+                  {
+                      "label": "May 2016"
+                  },
+                  {
+                      "label": "Jun 2016"
+                  },
+                  {
+                      "label": "Jul 2016"
+                  },
+                  {
+                      "label": "Aug 2016"
+                  },
+                  {
+                      "label": "Sep 2016"
+                  },
+                  {
+                      "label": "Oct 2016"
+                  },
+                  {
+                      "label": "Nov 2016"
+                  },
+                  {
+                      "label": "Dec 2016"
+                  },
+                  {
+                      "label": "Jan 2017"
+                  },
+                  {
+                      "label": "Feb 2017"
+                  },
+                  {
+                      "label": "Mar 2017"
+                  },
+                  {
+                      "label": "Apr 2017"
+                  },
+                  {
+                      "label": "May 2017"
+                  },
+                  {
+                      "label": "Jun 2017"
+                  },
+                  {
+                      "label": "Jul 2017"
+                  },
+                  {
+                      "label": "Aug 2017"
+                  },
+                  {
+                      "label": "Sep 2017"
+                  },
+                  {
+                      "label": "Oct 2017"
+                  },
+                  {
+                      "label": "Nov 2017"
+                  },
+                  {
+                      "label": "Dec 2017"
                   }
-                }],
-              }
-            }
-          })
-        );
-      }
-    
-      _randomYValues(range: number, size: number) {
-        const nextValueMaxDiff = 0.2;
-        let lastValue = range / 2;
-    
-        return _.times(size, () => {
-          let min = lastValue * (1 - nextValueMaxDiff);
-          let max = lastValue * (1 + nextValueMaxDiff);
-          return {y: Math.random() * (max - min) + min};
-        });
-      }
-    
-      handleSelect(event) {
-        let entry = event.nativeEvent
-        if (entry == null) {
-          this.setState({...this.state, selectedEntry: null})
-        } else {
-          this.setState({...this.state, selectedEntry: JSON.stringify(entry)})
-        }
-    
-        console.log(event.nativeEvent)
-      }
+              ]
+          }
+      ],
+      "dataset": [
+          {
+              "data": [
+                  {
+                      "value": "27400"
+                  },
+                  {
+                      "value": "29800"
+                  },
+                  {
+                      "value": "25800"
+                  },
+                  {
+                      "value": "26800"
+                  },
+                  {
+                      "value": "29600"
+                  },
+                  {
+                      "value": "32600"
+                  },
+                  {
+                      "value": "31800"
+                  },
+                  {
+                      "value": "36700"
+                  },
+                  {
+                      "value": "29700"
+                  },
+                  {
+                      "value": "31900"
+                  },
+                  {
+                      "value": "34800"
+                  },
+                  {
+                      "value": "24800"
+                  },
+                  {
+                      "value": "26300"
+                  },
+                  {
+                      "value": "31800"
+                  },
+                  {
+                      "value": "30900"
+                  },
+                  {
+                      "value": "33000"
+                  },
+                  {
+                      "value": "36200"
+                  },
+                  {
+                      "value": "32100"
+                  },
+                  {
+                      "value": "37500"
+                  },
+                  {
+                      "value": "38500"
+                  },
+                  {
+                      "value": "35400"
+                  },
+                  {
+                      "value": "38200"
+                  },
+                  {
+                      "value": "33300"
+                  },
+                  {
+                      "value": "38300"
+                  }
+              ]
+          }
+      ]
+    }
+    };
+
+    this.state = {
+      chartConfig
+    };
+  }
+
     
   render(){
     return (
       <View style={{flex: 1}}>
         <View style={styles.header}>
           <View style={styles.headerContent}>
-            <Text style={styles.title}>Playground</Text>
+            <Text style={styles.title}>Home</Text>
             <Text style={styles.description}>lorem ipsum dolor sit amet</Text>
           </View>
         </View>
          <View style={styles.content}>
           <View style={styles.mainContent}>
-            {/* <ScrollView> */}
-                <View>
-                    {/* <LineChart/> */}
-                {/* <LineChart
-            style={styles.chart}
-            data={this.state.data}
-            chartDescription={{text: ''}}
-            xAxis={this.state.xAxis}
-            yAxis={this.state.yAxis}
-            legend={{enabled: false}}
-            onSelect={this.handleSelect.bind(this)}
-            onChange={(event) => console.log(event.nativeEvent)}
-          /> */}
-                </View>
+            <ScrollView>
+              <View>
+                <View style={{
+                  width: '95%',
+                }}>
+                  <View style={{
 
-            {/* </ScrollView> */}
+                  }}>
+                      <View
+                        style={{
+                          paddingVertical: 100,
+                          backgroundColor: '#34448B',
+                          flex: 1,
+                        }}>
+                        <View
+                          style={{
+                            //margin: 20,
+                            padding: 16,
+                            borderRadius: 20,
+                            backgroundColor: '#232B5D',
+                          }}>
+                          <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold'}}>
+                            Bar Chart
+                          </Text>
+                          <View style={{}}>
+                          <ReactNativeFusionCharts
+                            style={{
+                              borderRadius: 20
+                            }}
+                            chartConfig={this.state.chartConfig}
+                          />
+                          </View>
+                        </View>
+                      </View>
+                      <View
+                        style={{
+                          paddingVertical: 100,
+                          backgroundColor: '#34448B',
+                          flex: 1,
+                        }}>
+                        <View
+                          style={{
+                            margin: 20,
+                            padding: 16,
+                            borderRadius: 20,
+                            backgroundColor: '#232B5D',
+                          }}>
+                          <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold'}}>
+                            Line Chart
+                          </Text>
+                          <View style={{padding: 20, alignItems: 'center'}}>
+                          {/* <LineChart 
+                            data={{dataSets:[{label: "demo", values: [{y: 1}, {y: 2}, {y: 1}]}]}}
+                          /> */}
+                          </View>
+                        </View>
+                      </View>
+                      <View
+                        style={{
+                          paddingVertical: 100,
+                          backgroundColor: '#34448B',
+                          flex: 1,
+                        }}>
+                        <View
+                          style={{
+                            margin: 20,
+                            padding: 16,
+                            borderRadius: 20,
+                            backgroundColor: '#232B5D',
+                          }}>
+                          <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold'}}>
+                            Pie Chart
+                          </Text>
+                          <View style={{padding: 20, alignItems: 'center'}}>
+
+                          </View>
+                        </View>
+                      </View>
+                      {/* 3D */}
+                      <View
+                        style={{
+                          paddingVertical: 100,
+                          backgroundColor: '#34448B',
+                          flex: 1,
+                        }}>
+                        <View
+                          style={{
+                            margin: 20,
+                            padding: 16,
+                            borderRadius: 20,
+                            backgroundColor: '#232B5D',
+                          }}>
+                          <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold'}}>
+                            3D Three
+                          </Text>
+                          <View style={{padding: 20, alignItems: 'center'}}>
+                            
+
+                          </View>
+                        </View>
+                      </View>
+                    </View>
+                </View>
+              </View>
+            </ScrollView>
           </View>
         </View> 
       </View>
@@ -164,3 +369,5 @@ const styles = StyleSheet.create({
     flex: 1
   }
 });
+
+AppRegistry.registerComponent('ReactNativeFusionCharts', () => PlayGround);
